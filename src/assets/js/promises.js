25041e4c40279a95726544e1ab58e6bfa2cdaf76
Naim Latifi <naimlatifi98@hotmail.com>
2018-06-08 22:10:51 +0200

    added example with promises, fetching users data

--- src/assets/js/promises.js
+++ src/assets/js/promises.js
@@ -94,6 +94,32 @@
 
    }
 
+   function getUserExample(url) {
+       //Return a new promise object
+       return new Promise((resolve, reject) => {
+       // Create a variable and assign a new XMLHttpRequest object to it.
+       let asyncReq = new XMLHttpRequest();
+       //Make request to url endpoint, using GET request.
+       asyncReq.open('GET', url);
+       asyncReq.onload = function() {
+          if (asyncReq.status == 200) {
+          // Recieved data, resolve promise.
+          resolve(asyncReq.response);
+          } else {
+          // Reject promise, wrong status.
+          reject(Error(asyncReq.statusText));
+          }
+        }
+        asyncReq.onerror = () => {
+         // Error occurred, reject Promise.
+         reject(Error("Network Error"));
+       }
+        // Send request
+        asyncReq.send();
+       });
+   }
+
+
     function initPromise() {
          // we call isGreaterThan
         isGreaterThanES5(5,2, function(result) {
@@ -120,5 +146,31 @@
         promiseCreation();
         console.log("======== Multiple promises =========");
         multiplePromisesInOne();
+        console.log("========= Fetching users example ========");
+        // make a request to fake rest api
+        getUserExample('https://jsonplaceholder.typicode.com/users/1')
+              .then((response) => {
+                // we return the data
+                return JSON.parse(response);
+              }).then((response) => {
+                  // we build the template to show the data
+               let template = '';
+               template += `
+                <div>
+                  <p>${response.name}</p>
+                  <p>${response.email}</p>
+                  <p>${response.website}</p>
+                </div>
+               `;
+               document.getElementById('appendUserData').innerHTML = template;
+              // we want to return a new promise but with another user
+              return getUserExample('https://jsonplaceholder.typicode.com/users/3')
+            }).then((responseUser3) => {
+                console.log(JSON.parse(responseUser3));
+            }).catch((error) => {
+              // change url request to something else to get a  catch erorr
+              console.log("Something went wrong", error);
+            })
+
     }
  initPromise();
