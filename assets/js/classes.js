ff2541310de6c4817059aefe20d7c5a4cb60bbcf
Naim Latifi <naimlatifi98@hotmail.com>
2018-11-08 22:52:55 +0100

    added file for ES6 without having a job task gulp

--- assets/js/classes.js
+++ assets/js/classes.js
@@ -1,37 +1,36 @@
-
 console.log("*********** From cunstructors to classes ***************");
 
-    function Person(name){
-       this.name = `Hello from constructor function ${name}`;
-    }
-    Person.prototype.displayName = function(){
-      return this.name;
-    }
+function Person(name) {
+  this.name = `Hello from constructor function ${name}`;
+}
+Person.prototype.displayName = function () {
+  return this.name;
+}
 
-    let person1 = new Person('Naim');
-    console.log(person1.displayName());
-    console.log("Is instance of Person ? ", person1 instanceof Person);
+let person1 = new Person('Naim');
+console.log(person1.displayName());
+console.log("Is instance of Person ? ", person1 instanceof Person);
 
 
 console.log("============== Classes ==============");
-  // class declaration
-  class Person1 {
-    // equivalent to the constructor function Person
-    constructor(name) {
-      this.name = `Hello from classes ES6 ${name}`;
-    }
-
-    displayName() {
-      return this.name;
-    }
+// class declaration
+class Person1 {
+  // equivalent to the constructor function Person
+  constructor(name) {
+    this.name = `Hello from classes ES6 ${name}`;
   }
+
+  displayName() {
+    return this.name;
+  }
+}
 
 // classes are not hoisted, calling person2 before class declaration throws an error.
 
-  let person2 = new Person1('Naim');
-  console.log(person2.displayName());
-  console.log(typeof Person1) // function
-  console.log("Is it instance of class Person1 ?" , person2 instanceof Person1);
+let person2 = new Person1('Naim');
+console.log(person2.displayName());
+console.log(typeof Person1) // function
+console.log("Is it instance of class Person1 ?", person2 instanceof Person1);
 
 
 // similarly to function declaration there can be two kinds of class declaration ; class expression and class declaration
@@ -44,10 +43,10 @@ const myClass1 = class {
 // class expression with name
 
 const myClass2 = class Me {
-   getClassName(){
-     console.log(typeof Me);
-     return Me.name;
-   }
+  getClassName() {
+    console.log(typeof Me);
+    return Me.name;
+  }
 }
 
 const myObjecClass = new myClass2();
@@ -60,13 +59,13 @@ console.dir(myObjecClass.getClassName());
 console.log("===============Inheritance =============");
 
 class Father1 {
-  constructor(name){
-  this.fatherName = "Anders";
-  this.name = name;
-  this.addresses = ['Söderberga', 'Nacka'];
+  constructor(name) {
+    this.fatherName = "Anders";
+    this.name = name;
+    this.addresses = ['Söderberga', 'Nacka'];
   }
   sayName() {
-    return  this.name;
+    return this.name;
   }
 
   sayFathersName() {
@@ -81,15 +80,15 @@ class Father1 {
 
 console.log("========== ES5 inheritance ========")
 class Child3 extends Father1 {
-    constructor(name,age){
-     // no need for stealing constructor here instad we use super
-      super(name)
-      this.age = age;
-    }
+  constructor(name, age) {
+    // no need for stealing constructor here instad we use super
+    super(name)
+    this.age = age;
+  }
 
-    getChildAge() {
-      return this.age;
-    }
+  getChildAge() {
+    return this.age;
+  }
 }
 
 console.log("child")
@@ -111,26 +110,26 @@ console.log(secondChild.getAddresses());
 
 console.log("========== WITH ES5 inheritance==========")
 
-function Father1(name){
+function Father2(name) {
   this.name = name;
   this.fathersName = 'Anders';
   this.addresses = ['Söderberga', 'Nacka'];
 }
-Father1.prototype.sayFatherName = function(){
+Father2.prototype.sayFatherName = function () {
   console.log(this.fathersName);
 }
 
-function Child3(name, age){
+function Child4(name, age) {
   // stealing constructor
-   Father1.call(this, name);
-   this.age = age;
+  Father2.call(this, name);
+  this.age = age;
 }
 // all properties
-Child3.prototype = new Father1();
-Child3.prototype.sayName = function(){
+Child4.prototype = new Father2();
+Child4.prototype.sayName = function () {
   return this.name;
 }
-var firstChildES5 = new Child3('Joe', 22);
+var firstChildES5 = new Child4('Joe', 22);
 console.log("First child ", firstChildES5);
 console.log(firstChildES5.sayFatherName());
 console.log(firstChildES5.sayName());
