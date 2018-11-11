9126be62e102c4a3267d99029ac445b274f6ed69
Naim Latifi <naimlatifi98@hotmail.com>
2018-11-11 16:29:08 +0100

    added challenge for arrow functions and default parameters

---
+++ src/assets/scripts/challengesES6/challenge_say_hello_spread_rest.js
@@ -0,0 +1,16 @@
+/* 
+    Rest and spread operator challenge 4 
+*/
+
+// 1. Modify the function below so that if the second parameter lastName is not given , display as default parameter to user that lastName is not given
+// 2. Discuss and what can you conclude with the ES6 solution
+// 3. 
+function displayUserInfo(name, lastName) {
+
+    return "user info: " + name + lastName;
+}
+console.log(displayUserInfo('Naim '));
+
+// --- write code/solution below ----
+
+// #Challenge 4.1
